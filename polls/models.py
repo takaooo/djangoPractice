7190bc5e99f22729d89ff07497595560b281bfff
takaooo <takao.kobayashi88@gmail.com>
2014-12-10 22:31:40 +0000

    end of tutorial 2

--- polls/models.py
+++ polls/models.py
@@ -7,10 +7,14 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date' #can order column/field
+    was_published_recently.boolean = True #is boolean
+    was_published_recently.short_description = 'Published recently?' #name of column
     def __str__(self):
         return self.question_text
-    def was_publichsed_recently(self):
-        return self.pub_date >= timezone.now() - date.timedelta(days=1)
+    
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
